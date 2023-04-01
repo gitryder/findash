874864e17eb0cca91250dd22c8415688081639a5
Danyl Fernandes <realllydan@gmail.com>
2023-04-01 22:51:51 +0530

    feat: add components to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Navbar, AtAGlance, NetSavings } from "@components";
 
 export default function Home() {
   return (
     <React.Fragment>
-      <main>author: dantheman</main>
+      <Navbar />
+      <main
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "1.75rem",
+          gap: "2.25rem",
+        }}
+      >
+        <NetSavings />
+        <AtAGlance />
+      </main>
     </React.Fragment>
   );
 }
