094dd9551990efd0c3cd37637ee225662061a96b
Danyl Fernandes <realllydan@gmail.com>
2023-04-02 03:11:06 +0530

    chore: add new components to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,9 @@ export { ColoredBox } from "./ColoredBox";
 export { BorderedBox } from "./BorderedBox";
 export { Text, H5, Label } from "./Text";
 export { Navbar } from "./Navbar";
+export { Button } from "./Button";
 
 // Derived
 export { AtAGlance } from "./AtAGlance";
 export { NetSavings } from "./NetSavings";
+export { CashFlow } from "./CashFlow";
