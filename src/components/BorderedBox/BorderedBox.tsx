e1034196e6a60cdd1b2aa9700594c0f078346f71
Danyl Fernandes <realllydan@gmail.com>
2023-04-02 03:09:44 +0530

    feat: add fixed flex direction to bordered box

--- src/components/BorderedBox/BorderedBox.tsx
+++ src/components/BorderedBox/BorderedBox.tsx
@@ -14,6 +14,7 @@ const BorderedBox = ({
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         gap: `${gap}rem`,
         padding: "1rem",
         border: `3px solid ${colors.grey.dark}`,
