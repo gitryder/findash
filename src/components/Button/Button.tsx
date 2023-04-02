3f1df3c4225fd9991cb45c186e85f6994d52e6b5
Danyl Fernandes <realllydan@gmail.com>
2023-04-02 22:03:41 +0530

    feat: add disabled state to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 
 const Button = ({
+  disabled,
   children,
   onClick,
 }: {
+  disabled?: boolean;
   children: React.ReactNode;
   onClick: React.ButtonHTMLAttributes<HTMLButtonElement>["onClick"];
 }) => {
   return (
-    <button onClick={onClick}>
+    <button onClick={onClick} disabled={disabled}>
       <style jsx>{`
         button {
+          width: 100%;
           padding: 14px;
           border-radius: 8px;
           background-color: hsl(0, 0%, 100%);
@@ -18,10 +21,16 @@ const Button = ({
           cursor: pointer;
         }
         button:hover {
-          background-color: hsl(0, 0%, 97%);
+          background-color: ${disabled ? "transparent" : "hsl(0, 0%, 97%)"};
         }
         button:active {
           background-color: hsl(0, 0%, 93%);
+        }
+        button:disabled {
+          opacity: 0.5;
+          cursor: not-allowed;
+          background-color: hsl(0, 0%, 93%);
+          border: 4px solid hsla(0, 0%, 89%, 1);
         }
       `}</style>
       {children}
