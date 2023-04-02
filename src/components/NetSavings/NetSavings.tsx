f397d41d6710c913350261b248bb3c808ae2df9e
Danyl Fernandes <realllydan@gmail.com>
2023-04-02 22:26:15 +0530

    feat: integrate net savings

--- src/components/NetSavings/NetSavings.tsx
+++ src/components/NetSavings/NetSavings.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { ColoredBox, Text, H5 } from "@components";
 
+import { useNetSavings } from "@queries/useNetSavings";
+
+import { getFormattedAmount } from "@helpers/amount";
+
 const NetSavings = () => {
+  const { data: savings } = useNetSavings();
+
   return (
     <ColoredBox direction="column" color="purple">
       <H5 color="purple">Your Net Savings</H5>
       <Text size={4} color="purple">
-        ₹21,983.89
+        {getFormattedAmount(savings)}
       </Text>
     </ColoredBox>
   );
