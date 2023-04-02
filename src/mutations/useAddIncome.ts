983d65a2f3f943e7a8ee3108dd013b4e68909322
Danyl Fernandes <realllydan@gmail.com>
2023-04-02 19:44:09 +0530

    feat: write implementation for adding income

---
+++ src/mutations/useAddIncome.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { API } from "@api";
+import { Income } from "@interfaces";
+
+import { incomeQueryKey } from "@queries/useIncome";
+
+const useIncome = () => {
+  const queryClient = useQueryClient();
+  return useMutation(
+    (income: Income) => {
+      return API.income.addIncome(income);
+    },
+    {
+      onSuccess: () => {
+        console.log("onSuccess");
+
+        queryClient.invalidateQueries(incomeQueryKey);
+      },
+    }
+  );
+};
+
+export { useIncome };
---
+++ src/pages/api/income/add.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Income } from "@interfaces";
+
+import { Supabase } from "@api";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
+  }
+
+  const income = req.body as Income;
+
+  await Supabase.from("income").insert({ ...income });
+
+  res.status(200);
+};
+
+export default handler;
