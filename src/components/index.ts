e1bf4a23d4f2c89b4ee798749baaf996c21e31fa
Danyl Fernandes <realllydan@gmail.com>
2023-04-02 22:05:44 +0530

    feat: add new primitives to export

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,8 @@ export { BorderedBox } from "./BorderedBox";
 export { Text, H5, Label } from "./Text";
 export { Navbar } from "./Navbar";
 export { Button } from "./Button";
+export { Dialog } from "./Dialog";
+export { Input } from "./Input";
 
 // Derived
 export { AtAGlance } from "./AtAGlance";
