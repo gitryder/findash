ab1f0da6bc572d5a477c86068cb550a953fe979e
Danyl Fernandes <realllydan@gmail.com>
2023-04-02 19:42:35 +0530

    feat: update api routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,33 +1,39 @@
+import axios from "axios";
+
 import { createClient } from "@supabase/supabase-js";
 import { Expense, Income } from "@interfaces";
+
+import { Configuration, OpenAIApi } from "openai";
 
 const Supabase = createClient(
   process.env.SUPABASE_URL ?? "",
   process.env.SUPABASE_KEY ?? ""
 );
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const OpenAI = new OpenAIApi(configuration);
+
 const API = {
   expenses: {
     getAllExpenses: () => {
-      return Supabase.from("expenses").select("*").order("created_at", {
-        ascending: false,
-      });
+      return Supabase.from("expenses").select();
     },
     addExpense: (expense: Expense) => {
-      return Supabase.from("expenses").insert({ ...expense });
+      return axios.post("/api/expense/add", expense);
     },
   },
 
   income: {
     getAllIncome: () => {
-      return Supabase.from("income").select("*").order("created_at", {
-        ascending: false,
-      });
+      return Supabase.from("income").select();
     },
     addIncome: (income: Income) => {
-      return Supabase.from("income").insert({ ...income });
+      return axios.post("/api/income/add", income);
     },
   },
 };
 
-export { API };
+export { API, Supabase, OpenAI };
